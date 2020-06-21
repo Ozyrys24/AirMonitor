a7924cc6be8626e012d121cbf4e4381bf88cb73f
Mateusz Bugaj <mateusz.bugaj@microsoft.wsei.edu.pl>
2020-06-21 15:15:27 +0200

    Add project files.

---
+++ AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace AirMonitor.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(false)]
+    public partial class DetailsPage : ContentPage
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Help_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Okno Pomocy", "Jeżeli potrzebujesz pomocy użyj googla", "Tak jest");
+        }
+    }
+}
